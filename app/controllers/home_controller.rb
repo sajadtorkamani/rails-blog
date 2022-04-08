e175d5623f780c0bf95672b5588d788db437845d
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-04-08 13:21:10 +0100

    WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
+  # GET /
   def index
+    if user_signed_in?
+      @posts = Post.where(user: current_user).order(created_at: :desc)
+      render "posts/index"
+    else
+      render :index
+    end
   end
 end
